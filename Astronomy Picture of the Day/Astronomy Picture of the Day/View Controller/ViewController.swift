896d7272c5bf19f6eba8c54fa2b919f7a55f6d07
fritzgt <tuanis@live.com>
2020-06-02 18:31:51 +0300

    working on collection view ui

---
+++ Astronomy Picture of the Day/Astronomy Picture of the Day/View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Astronomy Picture of the Day
+//
+//  Created by FGT MAC on 6/2/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
